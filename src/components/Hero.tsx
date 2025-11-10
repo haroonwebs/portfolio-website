80c9ee970ca666f60ffa423a56fa032c0c40a6ee
haroontalib <haroontalib8029@gmail.com>
2025-11-10 22:30:59 +0500

    font setup done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-purple-100 text-gray-800">
-      <h1 className="text-5xl font-bold mb-4">Hi, I’m Haroon 👋</h1>
-      <p className="text-xl">
-        A MERN & Next.js Developer crafting digital experiences
-      </p>
+    <div className="h-screen flex flex-col items-start justify-center bg-gradient-to-b from-gray-100 to-purple-100 text-gray-800">
+      <h1 className="text-5xl text- font-bold bg-gradient-to-r from-purple-800 via-black to-gray-800 bg-clip-text text-transparent mb-4 pl-10">
+        Muhammad Haroon Talib
+      </h1>
+      <p className="text-xl pl-10">Software Engineer</p>
     </div>
   );
 };
