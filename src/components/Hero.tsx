f8e5932153002ac5e5fe37cf103ce76e20fb4b2e
haroontalib <haroontalib8029@gmail.com>
2025-11-10 23:24:23 +0500

    Home page is done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex flex-col items-start justify-center bg-gradient-to-b from-gray-100 to-purple-100 text-gray-800">
-      <h1 className="text-5xl text- font-bold bg-gradient-to-r from-purple-800 via-black to-gray-800 bg-clip-text text-transparent mb-4 pl-10">
-        Muhammad Haroon Talib
-      </h1>
-      <p className="text-xl pl-10">Software Engineer</p>
+    <div className="relative h-screen flex items-center justify-center bg-gradient-to-b from-gray-100 to-purple-100">
+      {/* Profile Image */}
+      <Image
+        src="/haroon1.png"
+        alt="My Profile Picture"
+        width={750}
+        height={740}
+        className="ml-20 mb-20  opacity-50"
+      />
+
+      <div className="absolute h-screen flex flex-col items-start justify-center z-20">
+        <h1 className="text-5xl font-bold bg-gradient-to-r from-purple-800 via-black to-gray-800 bg-clip-text text-transparent mb-4 pl-10">
+          Muhammad Haroon Talib
+        </h1>
+        <p className="text-xl pl-10 text-gray-800">Software Engineer</p>
+      </div>
     </div>
   );
 };
